71b25b169c0c0880972f8dd7d93d454eefef2f8c
Mzkhan25 <mzkhan25@hotmail.com>
2015-10-06 22:38:38 +0500

    First commit

---
+++ JavaScript/FlyingEnemies.js
@@ -0,0 +1,23 @@
+﻿var imgObj = null;
+var animate;
+
+function init() {
+    imgObj = document.getElementById('enemies');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '1366px';
+    moveRight();
+}
+
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) - 10 + 'px';
+    if (parseInt(imgObj.style.left) < 0)
+        stop();
+    animate = setTimeout(moveRight, 20); // call moveRight in 20msec
+}
+
+function stop() {
+    clearTimeout(animate);
+    imgObj.style.left = '1366px';
+}
+
+window.onload = init;
---
+++ JavaScript/MovenmentScript.js
@@ -0,0 +1,17 @@
+﻿window.onkeydown = function (e) { if (!e) { e = window.event; } 
+   
+    var code = e.keyCode; 
+    var stickman = document.getElementById("player");
+    var top = parseInt (stickman.style.top, 10); var left = parseInt (stickman.style.left, 10); 
+    if ( code == 37 ) {   
+        if ( left > 0 ) { stickman.style.left = left - 10 + 'px'; } } 
+    else if ( code == 38 ) { 
+        if ( top > 0 ) { stickman.style.top = top - 10 + 'px'; } } else if ( code == 39 ) { 
+            if (left + stickman.width + 10 < window.innerWidth) { stickman.style.left = left + 10 + 'px'; }
+        } else if (code == 40) {
+            if (top + stickman.height + 10 < window.innerHeight) { stickman.style.top = top + 10 + 'px'; }
+        }
+}
+
+
+//This code has been taken from http://errietta.me/blog/move-an-image-around-with-arrow-keys/
